a53195c8ea2bad5c25b3084f51664664fb66d2b8
davidlobo <david@davidlobo.co.uk>
2025-03-13 15:39:18 +0000

    Cleaning up code

--- src/Controllers/DecomposerController.php
+++ src/Controllers/DecomposerController.php
@@ -8,7 +8,22 @@ use Fhsinchy\Inspire\Inspire;
 
 class DecomposerController
 {
-    public function __invoke(Decomposer $decomposer) {
+    public function __invoke(Decomposer $decomposer)
+    {
+        $decomposerApiKey = env('DECOMPOSER_API_KEY');
+        $apiKey = request()->get('apiKey');
+        $isAuthenticated = false;
+
+        if (!empty($decomposerApiKey)) {
+            if ($apiKey === $decomposerApiKey) {
+                $isAuthenticated = true;
+            }
+        }
+
+        if (!$isAuthenticated) {
+            abort(403);
+        }
+
         $packages = $decomposer->decompose();
 
         $phpVersion = phpversion();
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,4 +4,4 @@ use DavidLobo\Decomposer\Controllers\DecomposerController;
 use Fhsinchy\Inspire\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('inspire', DecomposerController::class);
+//Route::get('inspire', DecomposerController::class);
